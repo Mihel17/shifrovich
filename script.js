5d87fa1086adc27fb0d25bdee5755c95db560cad
Miguel <fedosieiev.mihel@gmail.com>
2021-10-28 23:00:13 +0300

    width

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ inputEncrypt.addEventListener("input", function (evt) {
       codedMessage[i] = 57;
     }
 
-    resultEncrypt.textContent = codedMessage;
+    resultEncrypt.textContent = codedMessage.join(', ');
     console.log(codedMessage);
   };
 
@@ -152,7 +152,7 @@ let resultDecipher = document.querySelector('.app__result-decipher');
 inputDecipher.addEventListener("input", function (evt) {
   evt.preventDefault();
   //-----------------------------------
-  let encodedSymbols = inputDecipher.value.split(',');
+  let encodedSymbols = inputDecipher.value.split(', ');
   //-----------------------------------
   let decodedMessage = '';
   let messageArray = [];
